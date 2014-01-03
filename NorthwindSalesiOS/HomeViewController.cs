f86ec4b656ce0ee7f92acbed7000e9f6725767cb
Prashant Cholachagudda <pvc@outlook.com>
2014-01-03 15:38:47 +0530

    More code refactoring

--- NorthwindSalesiOS/HomeViewController.cs
+++ NorthwindSalesiOS/HomeViewController.cs
@@ -5,13 +5,14 @@ using MonoTouch.Foundation;
 using System.Collections.Generic;
 using Simple.OData.Client;
 using Infragistics;
-using System.Drawing;
+using BigTed;
 
 namespace NorthwindSalesiOS
 {
 	public class HomeViewController : UIViewController
 	{
 		readonly ODataClient client;
+		IGChartView chart;
 
 		public HomeViewController ()
 		{
@@ -20,74 +21,74 @@ namespace NorthwindSalesiOS
 
 		public override void ViewDidLoad ()
 		{
-			this.Title = "Northwind Sales Dashboard";
-			this.EdgesForExtendedLayout = UIRectEdge.None;
+			Title = "Northwind Sales Dashboard";
+			EdgesForExtendedLayout = UIRectEdge.None;
 
-			IGChartView chart = null;
-
-
-			var fetchDataButton = new UIBarButtonItem ("Fetch Data", UIBarButtonItemStyle.Plain, async (object sender, EventArgs e) => {
-
-				// if chart was added previously, remove it from the view before constructing it with new values
-				if (chart != null)
-					chart.RemoveFromSuperview ();
-
-				chart = new IGChartView (this.View.Bounds);
-				chart.AutoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth;
-				chart.Theme = IGChartGradientThemes.IGThemeDark ();
-
-				var progressView = new IGProgressView (IGProgressViewStyle.IGProgressViewStyleRadialIndeterminate);
-				progressView.Frame = new RectangleF ((this.View.Bounds.Size.Width / 2) - 100, (this.View.Bounds.Size.Height / 2) - 100, 200, 200);
-				progressView.AutoresizingMask = UIViewAutoresizing.FlexibleTopMargin | UIViewAutoresizing.FlexibleBottomMargin | UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleRightMargin;
-			
-				this.View.Add (progressView);
-
-				var data = await GetDataAsync ();
-
-				// set data source
-				IGCategorySeriesDataSourceHelper barSeriesSource = new IGCategorySeriesDataSourceHelper ();
-				barSeriesSource.Values = data.ProductSales.ToArray ();
-				barSeriesSource.Labels = data.ProductName.ToArray ();
-
-			
-				// Create axis types and add it to the chart
-				IGNumericXAxis xAxisBar = new IGNumericXAxis ("xAxis");
-				IGCategoryYAxis yAxisBar = new IGCategoryYAxis ("yAxis");
-				yAxisBar.LabelAlignment = IGHorizontalAlign.IGHorizontalAlignRight;
-
-				chart.AddAxis (xAxisBar);
-				chart.AddAxis (yAxisBar);
-
-				// decide on what series need to be displayed on the chart
-				IGBarSeries barSeries = new IGBarSeries ("series");
-				barSeries.XAxis = xAxisBar;
-				barSeries.YAxis = yAxisBar;
-
-				// set the appropriate data sources
-				barSeries.DataSource = barSeriesSource;
-				chart.AddSeries (barSeries);
-
-				progressView.RemoveFromSuperview ();
-
-				this.View.Add (chart);
-
-
-			});
+			var fetchDataButton = new UIBarButtonItem ("Fetch Data", UIBarButtonItemStyle.Plain, OnFetchDataTap);
 
 			NavigationItem.SetRightBarButtonItem (fetchDataButton, false);	
 		}
 
+	
+
+		async void OnFetchDataTap (object sender, EventArgs e)
+		{
+			InitialiseChart ();
+
+			BTProgressHUD.Show ("Fetching data");
+
+			var data = await GetDataAsync ();
+
+			// set data source
+			var barSeriesSource = new IGCategorySeriesDataSourceHelper ();
+			barSeriesSource.Values = data.ProductSales.ToArray ();
+			barSeriesSource.Labels = data.ProductName.ToArray ();
+
+			// Create axis types and add it to the chart
+			var xAxisBar = new IGNumericXAxis ("xAxis");
+			var yAxisBar = new IGCategoryYAxis ("yAxis");
+			yAxisBar.LabelAlignment = IGHorizontalAlign.IGHorizontalAlignRight;
+
+			chart.AddAxis (xAxisBar);
+			chart.AddAxis (yAxisBar);
+
+			// decide on what series need to be displayed on the chart
+			var barSeries = new IGBarSeries ("series");
+			barSeries.XAxis = xAxisBar;
+			barSeries.YAxis = yAxisBar;
+
+			// set the appropriate data sources
+			barSeries.DataSource = barSeriesSource;
+			chart.AddSeries (barSeries);
+
+			BTProgressHUD.Dismiss ();
+
+
+		}
+
 		async Task<SalesByCategory> GetDataAsync ()
 		{
-			var salesByCategory = await client.For ("Sales_by_Categories").Top (15).OrderBy ("ProductSales").FindEntriesAsync ();
+			var salesByCategory = await client.For("Sales_by_Categories")
+				.Top (15).OrderBy ("ProductSales").FindEntriesAsync ();
 	
-			SalesByCategory saleByCtg = new SalesByCategory ();
+			var saleByCtg = new SalesByCategory ();
 			foreach (var sale in salesByCategory) {
 				saleByCtg.ProductName.Add (NSObject.FromObject (sale ["ProductName"].ToString ()));
 				saleByCtg.ProductSales.Add (NSObject.FromObject (sale ["ProductSales"].ToString ()));
 			}
 
 			return saleByCtg;
+		}
+
+		void InitialiseChart ()
+		{
+			// if chart was added previously, remove it from the view before constructing it with new values
+			if (chart != null)
+				chart.RemoveFromSuperview ();
+			chart = new IGChartView (View.Bounds);
+			chart.AutoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth;
+			chart.Theme = IGChartGradientThemes.IGThemeDark ();
+			View.Add (chart);
 		}
 	}
 
